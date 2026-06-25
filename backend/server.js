fb8e6208ae813cab94b59fa5deb08e841ec3755c
Hamid Rza <hamidrza0008@gmail.com>
2026-06-25 08:02:00 +0530

    hskj

--- backend/server.js
+++ backend/server.js
@@ -18,14 +18,11 @@ app.use(cors());
 app.use(express.json());
 
 app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://expanse-tracker-chi.vercel.app"
-  ],
+  origin: "https://expanse-tracker-chi.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
 }));
 
-app.options("*", cors());
 
 
 // Routes Integration
